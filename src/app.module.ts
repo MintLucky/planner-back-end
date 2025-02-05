4b2f8c2310628974133905f48619a479b339070e
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-02-05 20:44:10 -0300

    creation of time blocks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { TaskModule } from './task/task.module'
+import { TimeBlockModule } from './time-block/time-block.module'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		AuthModule,
 		UserModule,
-    TaskModule
+    TaskModule,
+    TimeBlockModule,
 	]
 })
 export class AppModule {}
